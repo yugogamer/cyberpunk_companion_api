4002b121e6cb82b7373f0f2c70e60fc1b1771596
yugogamer <57806524+yugogamer@users.noreply.github.com>
2022-08-07 18:44:45 +0200

    Create 20220807161141_user.sql

---
+++ migrations/20220807161141_user.sql
@@ -0,0 +1,23 @@
+-- Add migration script here
+CREATE TABLE IF NOT EXISTS accounts(
+    id SERIAL NOT NULL PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE NOT NULL,
+    email TEXT NOT NULL UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    created_on TIMESTAMP NOT NULL,
+    last_login TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS groupes(
+    id SERIAL NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+);
+CREATE TABLE IF NOT EXISTS accounts_to_user(
+    id_account INTEGER NOT NULL REFERENCES accounts(id),
+    id_groupe INTEGER NOT NULL REFERENCES groupes(id),
+    PRIMARY KEY (id_account, id_groupe)
+);
+CREATE TABLE IF NOT EXISTS invite_links(
+    token TEXT NOT NULL,
+    id_account INTEGER NOT NULL REFERENCES accounts(id),
+    id_groupe INTEGER NOT NULL REFERENCES groupes(id)
+);
