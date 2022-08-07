cc6ec363bb1fb64314f1cad856876c9be412b3bc
yugogamer <57806524+yugogamer@users.noreply.github.com>
2022-08-07 18:01:43 +0200

    init project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, middleware, App, HttpResponse, HttpServer};
+
+mod utils;
+
+#[get("/")]
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().body("data")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Compress::default())
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
